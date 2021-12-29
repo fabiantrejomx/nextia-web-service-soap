e4684b41a6f1bd7d9ce6a4365d8e90012128ca8e
ftrejo <fabiantrejo.p@gmail.com>
2021-12-29 14:48:25 -0600

    Missing files to update.

---
+++ src/main/java/com/fabian/todo/soap/task/UpdateTaskRequest.java
@@ -0,0 +1,118 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2021.12.29 a las 02:36:58 PM CST 
+//
+
+
+package com.fabian.todo.soap.task;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="subject" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="isDone" type="{http://www.w3.org/2001/XMLSchema}boolean"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "id",
+    "subject",
+    "isDone"
+})
+@XmlRootElement(name = "UpdateTaskRequest")
+public class UpdateTaskRequest {
+
+    @XmlElement(required = true)
+    protected String id;
+    @XmlElement(required = true)
+    protected String subject;
+    protected boolean isDone;
+
+    /**
+     * Obtiene el valor de la propiedad id.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Define el valor de la propiedad id.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setId(String value) {
+        this.id = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad subject.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * Define el valor de la propiedad subject.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSubject(String value) {
+        this.subject = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad isDone.
+     * 
+     */
+    public boolean isIsDone() {
+        return isDone;
+    }
+
+    /**
+     * Define el valor de la propiedad isDone.
+     * 
+     */
+    public void setIsDone(boolean value) {
+        this.isDone = value;
+    }
+
+}
