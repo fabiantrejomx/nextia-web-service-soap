27c38def787f87968fea5fe44e8c9dfa0762ad50
ftrejo <fabiantrejo.p@gmail.com>
2021-12-29 10:17:34 -0600

    Start - Up and running app

---
+++ src/main/java/com/fabian/todo/soap/TodoWsSoapApplication.java
@@ -0,0 +1,13 @@
+package com.fabian.todo.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWsSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWsSoapApplication.class, args);
+	}
+
+}
